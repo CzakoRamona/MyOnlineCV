826e68078a780358fd9a0190cc9b48a6d783f5be
Ramona <drvizitiuramona@gmail.com>
2020-12-08 12:47:18 +0200

    add

---
+++ my-app/src/components/showOff.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class ShowOff extends Component {
+    render() {
+        return (
+            <div><h1>Show Off Page</h1></div>
+        )
+    }
+}
+export default ShowOff;
