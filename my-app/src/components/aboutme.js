5b124650bf720bced1329e89c0c4c90c516f21ec
Ramona <drvizitiuramona@gmail.com>
2020-12-11 10:52:16 +0200

    showOff

--- my-app/src/components/aboutme.js
+++ my-app/src/components/aboutme.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 class AboutMe extends Component {
     render() {
         return (
-            <div><h1>About Me Page</h1></div>
+            <div className="aboutme-container">
+                <Grid className="about-grid">
+                    <div id="sky-container">
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                    </div>
+                    <Cell col={4} className="about-left">
+                        <p>I enjoy turning complex problems into simple, beautiful and
+                        intuitive designs.
+                        When I'm not coding, tweeting or pushing pixels,
+                        you'll find me dancing, drawing, day dreaming, cooking, gardening or singing.</p>
+                        <p> I like to hear what you want and imagine in your project and  make your dreams come truth, in a creative and inspired mode</p>
+                    </Cell>
+                    <Cell col={8} className="about-right">
+                        <p>“Life is full of beauty. Notice it. Notice the bumble bee, the small child, and the smiling faces. Smell the rain, and feel the wind. Live your life to the fullest potential, and fight for your dreams.”
+– Ashley Smith </p>
+                        <p>
+                            “You are never too old to set another goal or to dream a new dream.”
+– C.S. Lewis</p>
+                        <p>When life give's you lemons... make a lemonade or lemon cake?</p>
+                        <p>I studied and learned to bee a pediatrician,but in this pandemic times I rediscovered me, in creating beutifull web designs</p>
+                    </Cell>
+
+
+                </Grid>
+            </div>
         )
     }
 }
--- my-app/src/components/showOff.js
+++ my-app/src/components/showOff.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 class ShowOff extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 }
+    }
     render() {
         return (
-            <div><h1>Show Off Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Photos</Tab>
+                    <Tab>Dancing</Tab>
+                    <Tab>Drawing</Tab>
+                </Tabs>
+                <section>
+                    <div className="content">Content for the tab: {this.state.activeTab}</div>
+                </section>
+            </div>
         )
     }
 }
