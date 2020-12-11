6bcf6f73cf45d6f541087f9d2e20187aee6620f5
Ramona <drvizitiuramona@gmail.com>
2020-12-11 11:09:11 +0200

    add

--- my-app/src/components/showOff.js
+++ my-app/src/components/showOff.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import { Tabs, Tab, Grid, Cell } from "react-mdl";
 class ShowOff extends Component {
     constructor(props) {
         super(props);
         this.state = { activeTab: 0 }
+    }
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <div><h1>This is React</h1></div>
+            )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div><h1>This is Photos</h1></div>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (
+                <div><h1>This is Dancing</h1></div>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div><h1>This is Drawing</h1></div>
+            )
+        }
     }
     render() {
         return (
@@ -14,8 +33,13 @@ class ShowOff extends Component {
                     <Tab>Dancing</Tab>
                     <Tab>Drawing</Tab>
                 </Tabs>
-                <section>
-                    <div className="content">Content for the tab: {this.state.activeTab}</div>
+                <section className="showOff-grid">
+                    <Grid className="showOff-grid">
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}
+                            </div>
+                        </Cell>
+                    </Grid>
                 </section>
             </div>
         )
