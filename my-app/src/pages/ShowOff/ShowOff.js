c9d84751a1145eeeedd1a3c8fec6b0ea2089879d
Ramona <drvizitiuramona@gmail.com>
2020-12-18 17:59:29 +0200

    add

--- my-app/src/pages/ShowOff/ShowOff.js
+++ my-app/src/pages/ShowOff/ShowOff.js
@@ -5,7 +5,7 @@ import CoffeeCarousel from "../../components/photoSlider/CoffeeCarousel";
 import FoodCarousel from "../../components/photoSlider/FoodCarousel";
 import PlayerGame from "./PlayerGame.png";
 import DrBooBoo from "./DrBooBoo.png";
-import MyIMDB-16.2 from "./MyIMDB-16.2.png";
+import MyIMDB from "./MyIMDB.png";
 
 
 class ShowOff extends Component {
@@ -18,7 +18,7 @@ class ShowOff extends Component {
             return (
                 <div><h1>My IT projects</h1>
                     <div className="cards-container">
-                        <Card shadow={5} style={{ width: '512px', height: "600px", margin: 'auto' }}>
+                        <Card shadow={5} style={{ width: '600px', height: "600px", margin: 'auto' }}>
                             <CardTitle style={{ fontSize: "auto", color: " #c06c84 " }}> My first IT project-DrBooBoo page</CardTitle>
                             <div className="project-photo" >
                                 <img src={DrBooBoo} alt="first project" />
@@ -32,14 +32,12 @@ class ShowOff extends Component {
                                     <Button colored>GitHub</Button>
                                 </a>
                             </CardActions>
-                            <CardMenu style={{ color: '#fff' }}>
-                                <IconButton name="share" />
-                            </CardMenu>
+
                         </Card>
-                        <Card shadow={5} style={{ width: '512px', height: "600px", margin: 'auto' }}>
+                        <Card shadow={5} style={{ width: '600px', height: "600px", margin: 'auto' }}>
                             <CardTitle style={{ fontSize: "auto", color: " #c06c84 " }}>PlayerGame</CardTitle>
-                            <div>
-                                <img src={PlayerGame} alt="player game" className="project-photo" />
+                            <div className="project-photo" >
+                                <img src={PlayerGame} alt="player game" />
                             </div>
                             <CardText>
                                 In this project I used JavaScript ES6 and OOP style.
@@ -51,27 +49,23 @@ class ShowOff extends Component {
                                     <Button colored>GitHub</Button>
                                 </a>
                             </CardActions>
-                            <CardMenu style={{ color: '#fff' }}>
-                                <IconButton name="share" />
-                            </CardMenu>
+
                         </Card>
-                        <Card shadow={5} style={{ width: '512px', height: "600px", margin: 'auto' }}>
+                        <Card shadow={5} style={{ width: '600px', height: "600px", margin: 'auto' }}>
                             <CardTitle style={{ fontSize: "auto", color: " #c06c84 " }}> React Project-My IMD-16.2</CardTitle>
-                            <div>
-                                <img src={MyIMDB - 16.2} alt="IMDB-like app" className="project-photo" />
+                            <div className="project-photo" >
+                                <img src={MyIMDB} alt="IMDB-like app" />
                             </div>
                             <CardText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Mauris sagittis pellentesque lacus eleifend lacinia...
+                                We built a complete front-end app using React, that lets the user find and display information about their favorite movies.I worked mainly at the design,search component and pagination.
                         </CardText>
                             <CardActions border>
                                 <a href="https://github.com/razvantimis/myIMDB-16.2" rel="noopener noreferrer" target="_blank">
                                     <Button colored>GitHub</Button>
                                 </a>
                             </CardActions>
-                            <CardMenu style={{ color: '#fff' }}>
-                                <IconButton name="share" />
-                            </CardMenu>
+
+
                         </Card>
 
 
